8aa5a3db81aca84fcb399c3d12adc0c91772c2d2
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-07-26 12:06:12 +0500

    Commit task 3

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    
