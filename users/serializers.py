66ba145c985b94cf96750485dd9bbdcc5967f75d
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-07-27 13:07:34 +0500

    commit - displaying payment history for the user profile

--- users/serializers.py
+++ users/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'phone', 'city', 'avatar', 'payments',)
