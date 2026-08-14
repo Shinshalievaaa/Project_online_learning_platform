f396ea6fe8ebe7ccd6325a02804f6ef437d22d4a
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-08-14 20:20:26 +0500

    commit - task 2

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,18 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# # Для тестирования в консоли без реального SMTP-сервера:
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Почта отправителя
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
--- lms/tasks.py
+++ lms/tasks.py
@@ -1,6 +1,20 @@
 from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+from lms.models import CourseSubscription
 
 
 @shared_task
-def send_email(user_email, message):
-    pass
+def send_course_update_email(course_id, course_title):
+    """Отправляет уведомления об обновлении курса всем пользователям, подписанным на данный курс."""
+    # Находим все активные подписки на данный курс
+    subscriptions = CourseSubscription.objects.filter(course_id=course_id)
+    recipient_list = [sub.user.email for sub in subscriptions if sub.user.email and sub.user.email.strip()]
+    if recipient_list:
+        send_mail(
+            subject=f'Обновление курса: {course_title}',
+            message=f'Здравствуйте! Материалы курса "{course_title}", на который вы подписаны, были обновлены.',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=recipient_list,
+            fail_silently=False,
+        )
